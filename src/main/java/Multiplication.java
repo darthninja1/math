6e71343b022df4ed72fe9f151931c735bc5b9e9c
darthninja1 <nmalik1@gmail.com>
2019-04-24 18:20:41 -0400

    Multiplication

--- src/main/java/Multiplication.java
+++ src/main/java/Multiplication.java
@@ -2,39 +2,38 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
-public class Addition {
-    private static final int MAX = 11;
+public class Multiplication {
+    private static final int MAX = 13;
     private static Random rnd = new Random(System.currentTimeMillis());
 
     public static void main(String[] args) {
-        addingTo();
-        addingBy();
+        multiplyTo();
+        multiplyBy();
     }
 
-    private static void addingTo() {
+    private static void multiplyTo() {
         for (int i = 1; i < MAX; i++) {
-            System.out.println("Practice adding to " + i);
+            System.out.println("Practice multiplying to " + i);
             System.out.println();
             Set<Integer> set = new HashSet<>();
             for (int j = 0; j <= i; ) {
                 int num = rnd.nextInt(i + 1);
                 if (!set.contains(num)) {
-                    String formatted = String.format("%2d + %2d =  _____", i, num);
+                    String formatted = String.format("%2d  x %2d =  _____", i, num);
                     System.out.println(formatted);
                     set.add(num);
                     j++;
                 }
             }
             System.out.println();
-            System.out.println("Adding to " + i + " continued");
+            System.out.println("Multiplying to " + i + " continued");
             System.out.println();
             set.clear();
             for (int j = 0; j <= i; ) {
                 int num = rnd.nextInt(i + 1);
                 if (!set.contains(num)) {
-                    String formatted = String.format("%2d + _____ = %2d", i, i + num);
+                    String formatted = String.format("%2d  x _____ = %2d", i, i * num);
                     System.out.println(formatted);
-//                    System.out.println(i + " + " + "____ = " + (i + num));
                     set.add(num);
                     j++;
                 }
@@ -43,28 +42,28 @@ public class Addition {
         }
     }
 
-    private static void addingBy() {
+    private static void multiplyBy() {
         for (int i = 1; i < MAX; i++) {
-            System.out.println("Practice adding by " + i);
+            System.out.println("Practice multiplying by " + i);
             System.out.println();
             Set<Integer> set = new HashSet<>();
             for (int j = 0; j < MAX; ) {
                 int num = rnd.nextInt(MAX);
                 if (!set.contains(num)) {
-                    String formatted = String.format("%2d + %2d =  _____", num, i);
+                    String formatted = String.format("%2d  x %2d =  _____", num, i);
                     System.out.println(formatted);
                     set.add(num);
                     j++;
                 }
             }
             System.out.println();
-            System.out.println("Adding by " + i + " continued");
+            System.out.println("Multiplying by " + i + " continued");
             System.out.println();
             set.clear();
             for (int j = 0; j < MAX; ) {
                 int num = rnd.nextInt(MAX);
                 if (!set.contains(num)) {
-                    String formatted = String.format("____ + %2d =  %2d", i, i + num);
+                    String formatted = String.format("____  x %2d =  %3d", i, i * num);
                     System.out.println(formatted);
                     set.add(num);
                     j++;
--- src/main/java/Subtraction.java
+++ src/main/java/Subtraction.java
@@ -3,16 +3,18 @@ import java.util.Random;
 import java.util.Set;
 
 public class Subtraction {
+    private static final int MAX = 11;
+    private static Random rnd = new Random(System.currentTimeMillis());
+
     public static void main(String[] args) {
         subtractFrom();
     }
 
     private static void subtractFrom() {
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i < MAX; i++) {
             System.out.println("Practice subtracting from " + i);
             System.out.println();
             Set<Integer> set = new HashSet<>();
-            Random rnd = new Random(System.currentTimeMillis());
             for (int j = 0; j <= i; ) {
                 int num = rnd.nextInt(i + 1);
                 if (!set.contains(num)) {
